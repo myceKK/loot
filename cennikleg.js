88956f6290bd6f6b48c96fdf693e86c11801f11c
myceKK <xmycek@gmail.com>
2025-07-04 18:34:41 +0200

    Update cennikleg.js

--- cennikleg.js
+++ cennikleg.js
@@ -7,28 +7,25 @@
 
     clearInterval(interval);
 
-    const button = document.createElement("div");
-    button.id = "LegendButton";
-    button.title = "Cennik legend";
-    button.style.cssText = `
+    // GOBLIN BUTTON (Cennik legend)
+    const goblinButton = document.createElement("div");
+    goblinButton.title = "Cennik legend";
+    goblinButton.style.cssText = `
       width: 34px;
       height: 34px;
       position: absolute;
-      bottom: 54px;
+      bottom: 26px; /* niżej niż C */
       left: 1px;
       z-index: 298;
-      background: linear-gradient(135deg, #ff9900, #ffcc00);
-      padding: 1px;
-      border-radius: 4px;
       cursor: pointer;
       pointer-events: initial;
-      box-shadow: 0 0 4px rgba(0,0,0,0.5);
     `;
 
-    const img = document.createElement("img");
-    img.src = "https://micc.garmory-cdn.cloud/obrazki/npc/e2/gobsamurai.gif";
-    img.alt = "Cennik legend";
-    img.style.cssText = `
+    const goblinImg = document.createElement("img");
+    goblinImg.src =
+      "https://micc.garmory-cdn.cloud/obrazki/npc/e2/gobsamurai.gif";
+    goblinImg.alt = "Cennik legend";
+    goblinImg.style.cssText = `
       width: 100%;
       height: 100%;
       image-rendering: pixelated;
@@ -36,12 +33,11 @@
       border-radius: 2px;
     `;
 
-    button.appendChild(img);
-
-    button.addEventListener("click", () => {
+    goblinButton.appendChild(goblinImg);
+    goblinButton.addEventListener("click", () => {
       window.open("https://mycekk.github.io/loot/", "_blank");
     });
 
-    gameLayer.appendChild(button);
+    gameLayer.appendChild(goblinButton);
   }, 300);
 })();
